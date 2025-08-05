ed6599befb4f81a14e520fb97fcb9caa57d41415
AvinashGit <159417412+FreeGitProject@users.noreply.github.com>
2025-08-05 22:38:30 +0530

    CRUD With SP
    CRUD With SP

---
+++ CRUD.MVC.SP/Controllers/EmployeeController.cs
@@ -0,0 +1,69 @@
+﻿using CRUD.MVC.SP.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.MVC.SP.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDAL _dal;
+
+        public EmployeeController(IConfiguration configuration)
+        {
+            _dal = new EmployeeDAL(configuration);
+        }
+
+        public IActionResult Index()
+        {
+            return View(_dal.GetAll());
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_dal.GetById(id));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                _dal.Insert(emp);
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View(_dal.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                _dal.Update(emp);
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View(_dal.GetById(id));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _dal.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUD.MVC.SP/DataAccess/EmployeeDAL.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace CRUD.MVC.SP.DataAccess
+{
+    public class EmployeeDAL
+    {
+        private readonly string connectionString;
+
+        public EmployeeDAL(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public List<Employee> GetAll()
+        {
+            List<Employee> list = new List<Employee>();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_GetAllEmployees", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    list.Add(new Employee
+                    {
+                        Id = Convert.ToInt32(dr["Id"]),
+                        Name = dr["Name"].ToString(),
+                        Age = Convert.ToInt32(dr["Age"]),
+                        Department = dr["Department"].ToString()
+                    });
+                }
+            }
+            return list;
+        }
+
+        public Employee GetById(int id)
+        {
+            Employee emp = new Employee();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_GetEmployeeById", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    emp.Id = Convert.ToInt32(dr["Id"]);
+                    emp.Name = dr["Name"].ToString();
+                    emp.Age = Convert.ToInt32(dr["Age"]);
+                    emp.Department = dr["Department"].ToString();
+                }
+            }
+            return emp;
+        }
+
+        public void Insert(Employee emp)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_InsertEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Age", emp.Age);
+                cmd.Parameters.AddWithValue("@Department", emp.Department);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(Employee emp)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_UpdateEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", emp.Id);
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@Age", emp.Age);
+                cmd.Parameters.AddWithValue("@Department", emp.Department);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_DeleteEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
