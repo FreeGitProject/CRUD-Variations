c99c819957ba9527189fb2c89514ea12b689c09e
AvinashGit <159417412+FreeGitProject@users.noreply.github.com>
2025-08-15 13:50:29 +0530

    CRUD for Product Done
    in here we are create
    Create Model
    Create Data > ApplicationDbContext > Register
    AddDbContext<AppicationDbContext>
    Add-Migration
    Create repo than Controller than scafalding
    
    Step one CRUD for Product Done

---
+++ CRUD.EF.SP/Controllers/ProductController.cs
@@ -0,0 +1,91 @@
+﻿using CRUD.EF.SP.Models;
+using CRUD.EF.SP.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.EF.SP.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public ActionResult Index()
+        {
+            List<Product> products = _productRepository.GetAllProducts();
+            return View(products);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Product product = _productRepository.GetProduct(id);
+            return View(product);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //IFormCollection collection
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                _productRepository.CreateProduct(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Product product = _productRepository.GetProduct(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product product)//IFormCollection collection)
+        {
+            try
+            {
+                _productRepository.UpdateProduct(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Product product = _productRepository.GetProduct(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                _productRepository.DeleteProduct(id);   
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUD.EF.SP/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUD.EF.SP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.EF.SP.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+                
+        }
+       public DbSet<Product> Products { get; set; }
+    }
+}
